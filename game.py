2f7432e6ac670c0175991ff13ee3e6b6ec896709
unknown <justinchervony@gmail.com>
2022-08-19 20:39:02 -0400

    Initial commit. RunGame class and initial functions.

---
+++ game.py
@@ -0,0 +1,21 @@
+class RunGame:
+    def __init__(self):
+        pass
+    def GameStart():
+        print("Welcome to Rock Paper Scissors Lizard Spock")
+    def GameRules():
+        print("Here are the rules:")
+        print("Each match will be a 'best-of-three' series.")
+        print("Use the number keys indicated to enter your gesture selection!")
+        print("")
+        print("Scissors cut Paper")
+        print("Paper covers Rock")
+        print("Rock crushes Lizard")
+        print("Lizard poisons Spock")
+        print("Spock smashes Scissors")
+        print("Scissors decapitates Lizard")
+        print("Lizard eats Paper")
+        print("Paper disproves Spock")
+        print("Spock vaporizes Rock")
+        print("Rock crushes Scissors")
+        print("")
